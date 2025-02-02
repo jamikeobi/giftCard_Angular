26de57c79691763b7750ad05a06108866c8a8ade
morrhtechsolutions <joelubioka@gmail.com>
2025-02-02 21:33:42 +0100

    home-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,15 +6,18 @@ import { ContactComponent } from './pages/contact/contact.component';
 import { FaqsComponent } from './pages/faqs/faqs.component';
 import { PrivacyComponent } from './pages/privacy/privacy.component';
 import { TermsComponent } from './pages/terms/terms.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent }, // Default route to Landing Page
-  { path: '**', redirectTo: '', pathMatch: 'full' }, // Redirect unknown routes to Landing Page
   { path: 'home', component: HomeComponent },  // Default route
   { path: 'contact', component: ContactComponent },
   { path: 'faq', component: FaqsComponent},
   { path: 'privacy', component: PrivacyComponent},
   { path: 'terms', component: TermsComponent},
+  { path: 'navbar', component: NavbarComponent},
+  
+  { path: '**', redirectTo: '', pathMatch: 'full' }, // Redirect unknown routes to Landing Page
 ];
 
 @NgModule({
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  talkToTeam() {
+    alert('Redirecting to contact form...');
+  }
 
+  orderFavourites() {
+    alert('Redirecting to Order Favourites page...');
+  }
+
+  options = [
+    { image: '../../../assets/images/gift_1.jpg', buttonText: 'Order Digital' },
+    { image: '../../../assets/images/gift_2.jpg', buttonText: 'Find Out More' },
+    { image: '../../../assets/images/gift_3.jpg', buttonText: 'Read Here' },
+    { image: '../../../assets/images/gift_2.jpg', buttonText: 'Where to Spend' }
+  ];
 }
